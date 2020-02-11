9f1ddd28f233b6e556cc0a7becff6c22fa616747
bravemaster619 <bravemaster619@hotmail.com>
2020-02-11 18:49:19 +0800

    added workbox-cli

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   "globDirectory": "./",
+   "globPatterns": [
+      "assets/images/**.*"
+   ],
+   "swSrc": "src/sw.js",
+   "swDest": "sw.js",
+   "globIgnores": [
+      "./workbox-config.js"
+   ]
+};
